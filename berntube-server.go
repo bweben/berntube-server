b1bf13f68eaf04a7f4730ad894caa24fbd5ee747
Nathanael Weber <nathanael.weber@bbcag.ch>
2019-09-03 21:54:19 +0200

    fix: add cors options temporarily allow all

--- berntube-server.go
+++ berntube-server.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"github.com/bweben/berntube-server/config"
 	"github.com/bweben/berntube-server/web"
 	"github.com/plimble/ace"
+	"github.com/plimble/ace-contrib/cors"
 )
 
 func main() {
-	a := ace.New()
+	a := ace.Default()
+
+	a.Use(cors.Cors(config.CorsOptions))
+
 	a.GET("/api/v1/room/:id", web.RoomHandler)
 	a.GET("/api/v1/rooms", web.RoomsHandler)
-	a.Run(":8180")
+
+	a.Run(":5000")
 }
---
+++ config/options.go
@@ -0,0 +1,5 @@
+package config
+
+import "github.com/plimble/ace-contrib/cors"
+
+var CorsOptions = cors.Options{AllowOrigins: []string{"*"}}
