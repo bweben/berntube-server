859aa3237fe2ac7719bea5437ee41ff0501d149a
bweben <nathanael@ibw-informatik.ch>
2020-01-20 23:41:57 +0100

    refactor: change cors (v3)

--- web/helper/cors.go
+++ web/helper/cors.go
@@ -7,9 +7,10 @@ func CorsMiddleware(next http.Handler) http.Handler {
 		allowHeaders := "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization"
 
 		w.Header().Set("Content-Type", "application/json")
-		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:80")
 		w.Header().Set("Access-Control-Allow-Methods", "POST, PUT, PATCH, GET, DELETE")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		w.Header().Set("Access-Control-Allow-Headers", allowHeaders)
 
 		next.ServeHTTP(w, r)
